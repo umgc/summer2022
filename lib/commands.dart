0eb08824174e4c7028147403d8a6386c645089da
Zack Sloan <zack.d.sloan@gmail.com>
2022-06-24 16:53:29 -0400

    Initial implementation of UI from Figam

---
+++ lib/commands.dart
@@ -0,0 +1,14 @@
+class VoiceCommands {
+  List<String> commands = [
+    "help",
+    "links",
+    "next digest",
+    "settings",
+    "next",
+    "previous"
+  ];
+
+  List<String> commandList() {
+    return commands;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,263 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MailWidget());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MailWidget extends StatefulWidget {
+  @override
+  _MailWidgetState createState() => _MailWidgetState();
+}
 
-  // This widget is the root of your application.
+class _MailWidgetState extends State<MailWidget> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+    // Figma Flutter Generator MailWidget - FRAME
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return Container(
+        width: 375,
+        height: 760,
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(255, 255, 255, 1),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        child: Stack(textDirection: TextDirection.ltr, children: <Widget>[
+          Positioned(
+              top: 119,
+              left: 0,
+              child: Container(
+                  width: 375,
+                  height: 501,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                      bottomLeft: Radius.circular(20),
+                      bottomRight: Radius.circular(20),
+                    ),
+                    color: Color.fromRGBO(228, 228, 228, 0.6000000238418579),
+                  ))),
+          Positioned(
+              top: 498,
+              left: 56,
+              child: Container(
+                  width: 80,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                          color: Color.fromRGBO(0, 0, 0, 0.10000000149011612),
+                          offset: Offset(0, 4),
+                          blurRadius: 4)
+                    ],
+                    color: Color.fromRGBO(210, 208, 208, 1),
+                    borderRadius: BorderRadius.all(Radius.elliptical(80, 80)),
+                  ))),
+          Positioned(
+              top: 650,
+              left: 18,
+              child: Container(
+                  width: 90,
+                  height: 90,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                          color: Color.fromRGBO(0, 0, 0, 0.10000000149011612),
+                          offset: Offset(0, 4),
+                          blurRadius: 4)
+                    ],
+                    color: Color.fromRGBO(242, 242, 242, 1),
+                    borderRadius: BorderRadius.all(Radius.elliptical(90, 90)),
+                  ))),
+          Positioned(
+              top: 650,
+              left: 140,
+              child: Container(
+                  width: 90,
+                  height: 90,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                          color: Color.fromRGBO(0, 0, 0, 0.10000000149011612),
+                          offset: Offset(0, 4),
+                          blurRadius: 4)
+                    ],
+                    color: Color.fromRGBO(242, 242, 242, 1),
+                    borderRadius: BorderRadius.all(Radius.elliptical(90, 90)),
+                  ))),
+          Positioned(
+              top: 650,
+              left: 261,
+              child: Container(
+                  width: 90,
+                  height: 90,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                          color: Color.fromRGBO(0, 0, 0, 0.10000000149011612),
+                          offset: Offset(0, 4),
+                          blurRadius: 4)
+                    ],
+                    color: Color.fromRGBO(242, 242, 242, 1),
+                    borderRadius: BorderRadius.all(Radius.elliptical(90, 90)),
+                  ))),
+          Positioned(
+              top: 498,
+              left: 226,
+              child: Container(
+                  width: 80,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                          color: Color.fromRGBO(0, 0, 0, 0.10000000149011612),
+                          offset: Offset(0, 4),
+                          blurRadius: 4)
+                    ],
+                    color: Color.fromRGBO(210, 207, 207, 1),
+                    borderRadius: BorderRadius.all(Radius.elliptical(80, 80)),
+                  ))),
+          Positioned(
+              top: 37,
+              left: 161,
+              child: Text(
+                textDirection: TextDirection.ltr,
+                'Mail',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    color: Color.fromRGBO(0, 0, 0, 1),
+                    fontFamily: 'Roboto',
+                    fontSize: 36,
+                    letterSpacing:
+                        0 /*percentages not used in flutter. defaulting to zero*/,
+                    fontWeight: FontWeight.normal,
+                    height: 1.5 /*PERCENT not supported*/
+                    ),
+              )),
+          Positioned(
+              top: 200,
+              left: 14,
+              child: Container(
+                  width: 348,
+                  height: 107,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                          color: Color.fromRGBO(0, 0, 0, 0.10000000149011612),
+                          offset: Offset(0, 4),
+                          blurRadius: 4)
+                    ],
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/Image1.png'),
+                        fit: BoxFit.fitWidth),
+                  ))),
+          Positioned(
+            top: 418,
+            left: 28,
+            child: SvgPicture.asset('assets/images/rectangle4.svg',
+                semanticsLabel: 'rectangle4'),
+          ),
+          Positioned(
+              top: 429.5,
+              left: 59,
+              child: Text(
+                textDirection: TextDirection.ltr,
+                'Links',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Color.fromRGBO(0, 0, 0, 1),
+                    fontFamily: 'Roboto',
+                    fontSize: 24,
+                    letterSpacing:
+                        0 /*percentages not used in flutter. defaulting to zero*/,
+                    fontWeight: FontWeight.normal,
+                    height: 1),
+              )),
+          Positioned(
+            top: 418,
+            left: 180,
+            child: SvgPicture.asset('assets/images/rectangle5.svg',
+                semanticsLabel: 'rectangle5'),
+          ),
+          Positioned(
+              top: 429.5,
+              left: 189,
+              child: Text(
+                textDirection: TextDirection.ltr,
+                'All DetAILS',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Color.fromRGBO(0, 0, 0, 1),
+                    fontFamily: 'Roboto',
+                    fontSize: 24,
+                    letterSpacing:
+                        0 /*percentages not used in flutter. defaulting to zero*/,
+                    fontWeight: FontWeight.normal,
+                    height: 1),
+              )),
+          Positioned(
+              top: 267,
+              left: 21,
+              child: Container(
+                  width: 341,
+                  height: 126,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                      bottomLeft: Radius.circular(20),
+                      bottomRight: Radius.circular(20),
+                    ),
+                    color: Color.fromRGBO(232, 240, 244, 0.8999999761581421),
+                  ))),
+          Positioned(
+              top: 293,
+              left: -12,
+              child: Text(
+                textDirection: TextDirection.ltr,
+                ' QR: https://ww.A.web.site',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Color.fromRGBO(0, 0, 0, 1),
+                    fontFamily: 'Roboto',
+                    fontSize: 14,
+                    letterSpacing:
+                        0 /*percentages not used in flutter. defaulting to zero*/,
+                    fontWeight: FontWeight.normal,
+                    height: 1),
+              )),
+          Positioned(
+              top: 325,
+              left: -12,
+              child: Text(
+                textDirection: TextDirection.ltr,
+                'Bar: https://www.OTherWebSite.com',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Color.fromRGBO(0, 0, 0, 1),
+                    fontFamily: 'Roboto',
+                    fontSize: 14,
+                    letterSpacing:
+                        0 /*percentages not used in flutter. defaulting to zero*/,
+                    fontWeight: FontWeight.normal,
+                    height: 1),
+              )),
+          Positioned(
+              top: 354,
+              left: -9,
+              child: Text(
+                textDirection: TextDirection.ltr,
+                'URL: https://www.Example.com',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Color.fromRGBO(0, 0, 0, 1),
+                    fontFamily: 'Roboto',
+                    fontSize: 14,
+                    letterSpacing:
+                        0 /*percentages not used in flutter. defaulting to zero*/,
+                    fontWeight: FontWeight.normal,
+                    height: 1),
+              )),
+        ]));
   }
 }
