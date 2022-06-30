d1ed126d62eeae2e73614e5c02678952e332b129
Bgartrell <bgartrell@student.umgc.edu>
2022-06-30 15:24:24 -0400

    Added USPS Address Verification
    Added USPS Address Verification class and Unit Tests

---
+++ lib/usps_address_verification.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+import 'package:http/retry.dart';
+import 'package:xml/xml.dart' as xml;
+import 'package:http/http.dart' as http;
+
+
+//xml documentation: https://pub.dev/packages/xml
+//http documentation: https://pub.dev/packages/http
+//usps documentation: https://www.usps.com/business/web-tools-apis/address-information-api.pdf
+
+class UspsAddressVerification {
+
+  String strUrl = "https://secure.shippingapis.com/ShippingAPI.dll?API=Verify&XML=";
+  String strUserID = "974UNIVE7445";
+  String strXmlVersion = '"version="1.0"';
+
+  //Can't overload in dart so new Name for Json specific functions
+  Future<bool> verifyAddressString(String strAddr) async {
+    String strAddr1 = "";
+    String strCity = "";
+    String strState = "";
+    String strZip5 = "";
+    String strAddr2 = "";
+    String strZip4 = "";
+    RegExp numericEnd = RegExp( r'[0-9]+$');
+
+    try{
+
+    List<String> list = strAddr.split(';');
+
+    if(list.length == 3){
+      //assume that the second line is address 2
+      if(numericEnd.hasMatch(list[0])){
+        //line 1 is the second address line
+        strAddr2 = list[0].trim();
+        strAddr1 = list[1].trim();
+      }
+      else if(numericEnd.hasMatch(list[1])){
+        //line 2 has the second address value
+        strAddr2 = list[1].trim();
+        strAddr1 = list[0].trim();
+      }
+      if (list[2].contains(',')) {
+        //address is well formatted and can assume that left of the comma is the city
+        strCity = list[2].trim().split(',')[0];
+        strState = findState(list[2].trim());
+        strZip5 = findZip5(list[2].trim());
+        strZip4 = findZip4(list[2].trim());
+      }
+      else{
+        //address is not well formatted and need to find the other components before we can assume city
+        strState = findState(list[2].trim());
+        strZip5 = findZip5(list[2].trim());
+        strZip4 = findZip4(list[2].trim());
+        strCity = list[2].split(strState)[0].trim();
+      }
+    }
+    else if(list.length == 2) {
+      //assume that line one is address 1 and potentially address 2
+      if (numericEnd.hasMatch(list[0])) { //use to determine if the address line has a numerical value at the end of the string, this implies that there is a second address
+        final addrSplit = list[0].split(' ');
+        strAddr2 = '${addrSplit[addrSplit.length - 2]} ${addrSplit[addrSplit.length -1]}'; // address 2 can only be the last two values of the address line.
+        strAddr1 = list[0].split(strAddr2)[0]; //everything the left of address 2 will be address 1
+      }
+      else {
+        //there is no address 2
+        strAddr1 = list[0];
+      }
+
+      if (list[1].contains(',')) {
+        //address is well formatted and can assume that left of the comma is the city
+        strCity = list[1].trim().split(',')[0];
+        strState = findState(list[1].trim());
+        strZip5 = findZip5(list[1].trim());
+        strZip4 = findZip4(list[1].trim());
+      }
+      else{
+        //address is not well formatted and need to find the other components before we can assume city
+        strState = findState(list[1].trim());
+        strZip5 = findZip5(list[1].trim());
+        strZip4 = findZip4(list[1].trim());
+        strCity = list[1].split(strState)[0].trim();
+      }
+    }
+    else{
+      throw Exception('Address format not considered for validation');
+    }
+
+    xml.XmlDocument doc = buildAddressXml(strAddr1, strAddr2, strCity, strState, strZip5, strZip4);
+    //according to documentation, append xml to the end of the url
+    String strUri = strUrl + doc.outerXml;
+    //create web client
+    final client = RetryClient(http.Client());
+    //download response string from api
+    String strResponse = await client.read(Uri.parse(strUri));
+
+    if(strResponse.contains('<DPVConfirmation>')){
+      final responseSplit = strResponse.split('<DPVConfirmation>');
+      return responseSplit[1].characters.elementAt(0) == 'Y';
+    }
+    return false;
+    }
+    catch(e){
+      rethrow;
+    }
+  }
+
+  String findZip5(String str){
+    RegExp fiveAndFour = RegExp(r'[0-9]+-[0-9]+$');
+    RegExp numericEnd = RegExp( r'[0-9]+$');
+    if(fiveAndFour.hasMatch(str)){
+      final strSplit = str.split(' ');
+      return strSplit.last.split('-')[0];
+    }
+    else if(numericEnd.hasMatch(str)){
+      return str.split(' ').last;
+    }
+    else {
+      return '';
+    }
+  }
+
+  String findZip4(String str){
+    RegExp fiveAndFour = RegExp(r'[0-9]+-[0-9]+$');
+    if(fiveAndFour.hasMatch(str)){
+      final strSplit = str.split(' ');
+      return strSplit.last.split('-')[1];
+    }
+    return '';
+  }
+
+  String findState(String str){
+    RegExp shortState = RegExp(r' [A-Z]{2} ');
+    String strState = '';
+    if(shortState.hasMatch(str)){
+      str.split(' ').forEach((e) {
+        if(e.length == 2){
+          strState = e;
+        }
+      });
+    }
+    else{
+      //state value is not two characters so we must find zip as state is always before zip
+      final strSplit = str.split(' ');
+      for(int x = 0; x < strSplit.length; x++){
+        if(strSplit[x].contains('-') && int.tryParse(strSplit[x].split('-')[0]) != null){
+          strState = strSplit[x-1];
+          break;
+        } else if(int.tryParse(strSplit[x]) != null){
+          strState = strSplit[x-1];
+          break;
+        }
+      }
+    }
+    return strState;
+  }
+
+  xml.XmlDocument buildAddressXml(String strAddr1, String strAddr2, String strCity, String strState, String strZip5, String strZip4) {
+    final builder = xml.XmlBuilder();
+    builder.processing('xml', strXmlVersion);
+    builder.element('AddressValidateRequest', nest:() {
+      builder.attribute('USERID', strUserID);
+      builder.element('Revision', nest: 1);
+      builder.element('Address', nest:() {
+        builder.attribute('ID', 0);
+        builder.element('Address1', nest: strAddr1);
+        builder.element('Address2', nest: strAddr2);
+        builder.element('City', nest: strCity);
+        builder.element('State', nest: strState); //works with both full state names and abbreviation
+        builder.element('Zip5', nest: strZip5);
+        builder.element('Zip4', nest: strZip4);
+      });
+    });
+    return builder.buildDocument();
+  }
+}
---
+++ test/usps_address_validation_test.dart
@@ -0,0 +1,113 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:usps_informed_delivery_backend/usps_address_verification.dart';
+import 'package:xml/xml.dart' as xml;
+
+void main() async {
+    bool result;
+    final uspsAddressValidator = UspsAddressVerification();
+
+    group('USPS Address Verification Tests', () { 
+        test('Well Formated Address', () async {
+            const strAddress = r'1004 SPA RD, APT 202; ANNAPOLIS, MD 21403';
+            result = await uspsAddressValidator.verifyAddressString(strAddress);
+            print(result);
+            expect(result, true);
+        });
+        test('Well Formated Address, Without second Address', () async {
+            const strAddress = r'13001 VANDALIA DR; ROCKVILLE, MD 20853-3348';
+            result = await uspsAddressValidator.verifyAddressString(strAddress);
+            print(result);
+            expect(result, true);
+        });
+        test('No comma address', () async {
+            const strAddress = r'1004 SPA RD APT 202; ANNAPOLIS MD 21403';
+            result = await uspsAddressValidator.verifyAddressString(strAddress);
+            print(result);
+            expect(result, true);
+        });
+        test('Second address is on second line', () async {
+            const strAddress = r'1004 SPA RD; APT 202; ANNAPOLIS MD 21403';
+            result = await uspsAddressValidator.verifyAddressString(strAddress);
+            print(result);
+            expect(result, true);
+        });
+        test('Second address is on first line', () async {
+            const strAddress = r'APT 202; 1004 SPA RD; ANNAPOLIS MD 21403';
+            result = await uspsAddressValidator.verifyAddressString(strAddress);
+            print(result);
+            expect(result, true);
+        });
+        test('Second address is on first line with comma after city', () async {
+            const strAddress = r'APT 202; 1004 SPA RD; ANNAPOLIS, MD 21403';
+            result = await uspsAddressValidator.verifyAddressString(strAddress);
+            print(result);
+            expect(result, true);
+        });
+        test('PO Box address', () async {
+            const strAddress3 = r'PO Box 85149; Richmond, VA 23295-0001';
+            result = await uspsAddressValidator.verifyAddressString(strAddress3);
+            print(result);
+            expect(result, true);
+        });
+        test('Bad Address', () async {
+            try{
+                const strAddress = r'22 zzzzzzzzzzzzz zzzzzzzz zzzzzzzzzzz zzzzzzzzzzzzzz zzzzzzzzzzz';
+                result = await uspsAddressValidator.verifyAddressString(strAddress);
+                print(result);
+                expect(result, false);
+            }
+            catch(e){
+                print(e.toString());
+                expect(e, e);
+            }
+        });
+    });
+
+    group('USPS Address Validation Function Tests', () {
+        test('XML Builder Test', (){
+            xml.XmlDocument doc = uspsAddressValidator.buildAddressXml('1004 SPA RD', 'APT 202', 'ANNAPOLIS', 'MD', '21403', '');
+            print(doc.toString());
+            expect(doc.toString(), '<?xml "version="1.0"?><AddressValidateRequest USERID="974UNIVE7445"><Revision>1</Revision><Address ID="0"><Address1>1004 SPA RD</Address1><Address2>APT 202</Address2><City>ANNAPOLIS</City><State>MD</State><Zip5>21403</Zip5><Zip4></Zip4></Address></AddressValidateRequest>');
+        });
+        test('Find Zip5 from long zip', (){
+            final zip5 = uspsAddressValidator.findZip5('ANNAPOLIS MD 21403-5555');
+            print(zip5);
+            expect(zip5, '21403');
+        });
+        test('Find Zip5 from short zip', (){
+            final zip5 = uspsAddressValidator.findZip5('ANNAPOLIS MD 21403');
+            print(zip5);
+            expect(zip5, '21403');
+        });
+        test('Find Zip5 from no zip', (){
+            final zip5 = uspsAddressValidator.findZip5('ANNAPOLIS MD');
+            print(zip5);
+            expect(zip5, '');
+        });
+        test('Find Zip4 from long zip', (){
+            final zip4 = uspsAddressValidator.findZip4('ANNAPOLIS MD 21403-5555');
+            print(zip4);
+            expect(zip4, '5555');
+        });
+        test('Find Zip4 from short zip', (){
+            final zip4 = uspsAddressValidator.findZip4('ANNAPOLIS MD 21403');
+            print(zip4);
+            expect(zip4, '');
+        });
+        test('Find State from Short State', () {
+            final state = uspsAddressValidator.findState('ANNAPOLIS MD 21403-5555');
+            print(state);
+            expect(state, 'MD');
+        });
+        test('Find State from Long State', () {
+            final state = uspsAddressValidator.findState('ANNAPOLIS MARYLAND 21403');
+            print(state);
+            expect(state, 'MARYLAND');
+        });
+        test('Find State from Long State, with long zip', () {
+            final state = uspsAddressValidator.findState('ANNAPOLIS MARYLAND 21403-5555');
+            print(state);
+            expect(state, 'MARYLAND');
+        });
+    });
+}
