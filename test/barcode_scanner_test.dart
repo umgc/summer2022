831b4ab397fe8a5b59669c2927262caec9d6c526
sriniva5 <aasrin99@gmail.com>
2022-07-03 13:54:12 -0400

    Add test

---
+++ test/barcode_scanner_test.dart
@@ -0,0 +1,10 @@
+import 'package:test/test.dart';
+import '../lib/barcode_scanner.dart';
+
+void main() {
+  test('Scanner must return QR Code link', () {
+    final barcodeScanner = BarcodeScannerApi();
+
+    //expect(barcode.value, 1);
+  });
+}
